c0a4a5e5e0fd950746bf4c06013eed7c97d28b50
NeoTim <ceo@tml.ink>
2018-09-27 07:41:40 -0700

    tim.X('read').Set(a.id, {       like: a.innerHTML     });

---
+++ Backup.rar/新版本/caches/caches_commons/caches_data/sitelist.cache.php
@@ -0,0 +1,30 @@
+<?php
+return array (
+  1 => 
+  array (
+    'siteid' => '1',
+    'name' => '山东国软',
+    'dirname' => '',
+    'domain' => 'http://test4.c2chome.net/',
+    'site_title' => '国软软件',
+    'keywords' => '国软软件',
+    'description' => '国软软件',
+    'release_point' => '',
+    'default_style' => 'default',
+    'template' => 'default',
+    'setting' => 'array (
+  \'upload_maxsize\' => \'2048\',
+  \'upload_allowext\' => \'jpg|jpeg|gif|bmp|png|doc|docx|xls|xlsx|ppt|pptx|pdf|txt|rar|zip|swf\',
+  \'watermark_enable\' => \'0\',
+  \'watermark_minwidth\' => \'300\',
+  \'watermark_minheight\' => \'300\',
+  \'watermark_img\' => \'statics/images/water//mark.png\',
+  \'watermark_pct\' => \'85\',
+  \'watermark_quality\' => \'80\',
+  \'watermark_pos\' => \'9\',
+)',
+    'uuid' => '5df6a81f-7ba7-11e5-8676-4437e61cd7e3',
+    'url' => 'http://test4.c2chome.net/',
+  ),
+);
+?>
